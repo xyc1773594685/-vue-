fefb50204c0bcf3404f9a42db9bd80d14baa3d18
xyc1773594685 <138358010+xyc1773594685@users.noreply.github.com>
2026-02-25 14:31:36 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2026-02-24 13:24:14
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2026-02-24 16:49:10
+ * @LastEditTime: 2026-02-25 14:29:55
  * @FilePath: \Vue_JavaScript\myvue_class01\my-first-vue\src\JS\webpack.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+const { watchFile } = require('fs');
 const path = require('path');
+const { text } = require('stream/consumers');
+const { Static } = require('vue');
 // 正确引入 webpack 插件：从 webpack 模块中解构出 ProvidePlugin
 const { ProvidePlugin } = require('webpack');
 
@@ -20,6 +23,13 @@ module.exports = {
     clean: true // 打包前清空 dist 目录
   },
   mode: 'development',
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+    static: path.resolve(__dirname, './'),
+    watchFiles: ['./index.html', './index.js']
+  },
   // 插件配置（修复构造函数问题）
   plugins: [
     // 正确创建 ProvidePlugin 实例（解决 jQuery 全局注入+环境问题）
@@ -40,8 +50,17 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test: /\.less$/,
+        use:['style-loader','css-loader', 'less-loader']
       }
-    ]
+    ],
+
   },
   // 解决 jQuery 浏览器环境问题的兜底配置
   externals: {
